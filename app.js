8c948f3a80896548dd31ca02f75bebbf90f81deb
Shawn Pruden <shawnpruden@MacBook-Pro-3.local>
2024-06-12 10:40:27 +0900

    handling different routes

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {
-  console.log('middleware');
-
-  next();
+app.use('/dashboard', (req, res, next) => {
+  res.send('<h1>Dashboard</h1>');
 });
 
-app.use((req, res) => {
+app.use('/', (req, res) => {
   res.send('<h1>Hello, world!</h1>');
 });
 
