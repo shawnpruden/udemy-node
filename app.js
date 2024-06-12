e8bda446196272ba329d1a8e12e4845d0928d5ef
Shawn Pruden <shawnpruden@MacBook-Pro-3.local>
2024-06-12 10:08:19 +0900

    adding middleware

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use((req, res, next) => {
+  console.log('middleware');
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 const server = http.createServer(app);
-server.listen(3000);
+server.listen(3000, () => {
+  console.log('server is running!');
+});
