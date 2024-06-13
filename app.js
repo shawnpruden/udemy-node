0f28cf5dbfdc23e843d7289156a42d8a27ced0ee
Shawn Pruden <shawnpruden@MacBook-Pro-3.local>
2024-06-13 10:45:53 +0900

    adding a 404 error page

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req, res, next) => res.status(404).send('<h2>Page Not Found</h2>'));
 
 app.listen(3000, () => console.log('Server is running!'));
