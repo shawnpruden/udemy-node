34388acd150e3a758634dbd7b0d255ea171d2a06
Shawn Pruden <shawnpruden@MacBook-Pro-3.local>
2024-06-13 10:12:22 +0900

    parsing incoming requests

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
 const app = express();
-app.use('/dashboard', (req, res, next) => {
-  res.send('<h1>Dashboard</h1>');
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="post"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res) => {
-  res.send('<h1>Hello, world!</h1>');
+  res.send('<h1>Home</h1>');
 });
 
 app.listen(3000, () => console.log('Server is running!'));
