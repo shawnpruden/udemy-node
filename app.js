4c9126f59b917c099faab6d8406d2ed2af19c6a8
Shawn Pruden <shawnpruden@MacBook-Pro-3.local>
2024-06-14 09:58:07 +0900

    serving html pages

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -6,6 +8,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use((req, res, next) => res.status(404).send('<h2>Page Not Found</h2>'));
+app.use((req, res, next) =>
+  res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'))
+);
 
 app.listen(3000, () => console.log('Server is running!'));
