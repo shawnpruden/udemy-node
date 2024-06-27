4e140f0c00e1087ae440151d6a3ed53db4e896d3
Shawn Pruden <shawnpruden@MacBook-Pro-3.local>
2024-06-27 10:11:52 +0900

    storing data in files via model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../utils');
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +9,26 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const filePath = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(filePath, (error, fileContent) => {
+      let products = [];
+      if (!error) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+
+      fs.writeFile(filePath, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const filePath = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(filePath, (error, fileContent) => {
+      if (error) return [];
+
+      return JSON.parse(fileContent);
+    });
   }
 };
