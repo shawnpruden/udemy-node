55d319b06f16d3764f3e77567745035d5a9e2a0f
Shawn Pruden <shawnpruden@MacBook-Pro-3.local>
2024-07-05 10:08:06 +0900

    use query params

--- routes/admin.js
+++ routes/admin.js
@@ -4,8 +4,9 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/add-product', adminController.getAddProduct);
 router.get('/products', adminController.getProducts);
+router.get('/add-product', adminController.getAddProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/add-product', adminController.postAddProduct);
 
