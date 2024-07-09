5775e7c4697bdf7e6417bb871386bbde0dad3aa0
shawnpruden <echt.shawn@gmail.com>
2024-07-09 10:50:52 +0900

    delete cart items

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,10 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
+
+router.post('/cart', shopController.postCart);
+router.post('/cart-delete-item', shopController.postCartDeleteItem);
 
 module.exports = router;
