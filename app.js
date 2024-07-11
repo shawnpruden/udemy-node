c9394d103d8abe407450f98f67a7f2590f7b7cef
shawnpruden <echt.shawn@gmail.com>
2024-07-11 09:45:27 +0900

    connect app to sql database

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const path = require('path');
 const errorController = require('./controllers/error');
 
 const rootDir = require('./utils');
+const db = require('./database');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+db.execute('SELECT * FROM products');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(rootDir, 'public')));
