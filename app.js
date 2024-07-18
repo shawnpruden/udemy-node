9e031a278ddca230c6ccac656b8dbaa49240d4af
shawnpruden <echt.shawn@gmail.com>
2024-07-18 09:30:33 +0900

    create and manage a dummy user

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(rootDir, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => console.error(error));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
@@ -26,8 +35,16 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 sequelize
-  .sync({ force: true })
-  .then((results) => {
+  .sync()
+  .then(() => User.findByPk(1))
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'Shawn', email: 'shawn@gmail.com' });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(3000);
   })
   .catch((error) => console.error(error));
